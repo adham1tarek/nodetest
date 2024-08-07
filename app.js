2bac8faf1318a153d909a13104e485a0cb4675cb
Your Name <adhomydedo11@gmail.com>
2024-08-07 17:05:31 +0300

    commit2

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 
-// Body Parser Middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// Set up Pug as the view engine
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
-(async () => {
-  const expressHandlebars = await import('express-handlebars');
-  const hbs = expressHandlebars.create();
+// Define a route
+app.get('/', (req, res) => {
+  res.render('home', { title: 'Home Page' });
+});
 
-  // Set Handlebars as the template engine
-  app.engine('handlebars', hbs.engine);
-  app.set('view engine', 'handlebars');
-
-  // Define a static folder
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  // Define a route to render a view
-  app.get('/', (req, res) => {
-    res.render('home', { title: 'Home Page' });
-  });
-
-  const PORT = process.env.PORT || 5437;
-  app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-})();
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
